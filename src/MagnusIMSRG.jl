035657fd4df79c68eade02ed1eacea794c141609
Nicholas Todoroff <loppy1243@gmail.com>
2018-08-14 17:03:38 -0400

    Commit for posterity

--- src/MagnusIMSRG.jl
+++ src/MagnusIMSRG.jl
@@ -1,29 +1,35 @@
 module MagnusIMSRG
 
 using ManyBody
+using OrdinaryDiffEq
 using JuliaUtil: cartesian_pow, bernoulli
 
 const SPBASIS = Bases.Index{Bases.Pairing{4}}
 const REFSTATE = RefStates.Fermi{2, Bases.Pairing{4}}
 const MBBASIS = Bases.Paired{2, 4}
+const ARRAYOP{N} = F64ArrayOperator{N, SPBASIS}
+const FUNCOP{N} = F64FunctionOperator{N, SPBASIS}
 
-const CArrOperator{N} = Operator{N, SPBASIS, <:Array{Complex}}
+const TwoBodyARRAYOP = Tuple{ARRAYOP{0}, ARRAYOP{1}, ARRAYOP{2}}
 
 isocc(x) = ManyBody.isocc(REFSTATE, x)
 
 nbody(A, n) = A[n-1]
+nbodyrep(A, n) = rep(nbody(A, n))
 
-## How to structure this? We want tabulate() to propragate through the calculation after it is
-## called *once*.
-comm2(A, B) = (_comm0(A, B), _comm1(A, B),  _comm2(A, B))
-_comm0(A, B) = _comm0_2_2(nbody(A, 2), nbody(B, 2))
-_comm1(A, B, i, j) =
-    _comm1_1_2(nbody(A, 1), nbody(B, 2), i, j) + _comm1_2_2(nbody(A, 2), nbody(B, 2), i, j) #=
- =# - _comm1_1_2(nbody(B, 1), nbody(A, 2), i, j)
-_comm2(A, B, i, j, k, l) =
-    _comm2_1_2(nbody(A, 1), nbody(B, 2), i, j, k, l) #=
- =# + _comm2_2_2(nbody(A, 2), nbody(B, 2), i, j, k, l) #=
- =# + _comm2_1_2(nbody(B, 1), nbody(A, 2), i, j, k, l)
+comm2(A::TwoBodyARRAYOP, B::TwoBodyARRAYOP) =
+    (_comm0(BT, A, B), tabulate(_comm1(A, B)), tabulate(_comm2(A, B)))
+_comm0(A, B) = ARRAYOP{0}(fill(_comm0_2_2(nbodyrep(A, 2), nbodyrep(B, 2))))
+_comm1(A, B) = FUNCOP{1}() do i, j
+    _comm1_1_2(nbodyrep(A, 1), nbodyrep(B, 2), i, j) #=
+ =# + _comm1_2_2(nbodyrep(A, 2), nbodyrep(B, 2), i, j) #=
+ =# - _comm1_1_2(nbodyrep(B, 1), nbodyrep(A, 2), i, j)
+end
+_comm2(A, B) = FUNCOP{2}() do i, j, k, l
+    _comm2_1_2(nbodyrep(A, 1), nbodyrep(B, 2), i, j, k, l) #=
+ =# + _comm2_2_2(nbodyrep(A, 2), nbodyrep(B, 2), i, j, k, l) #=
+ =# + _comm2_1_2(nbodyrep(B, 1), nbodyrep(A, 2), i, j, k, l)
+end
 
 _comm0_2_2(A, B) = 4 \ sum(cartesian_pow(SPBASIS, Val{4})) do I
     a, b, c, d = I
@@ -54,7 +60,7 @@ _comm2_2_2(A, B, i, j, k, l) = 4 \ sum(cartesian_pow(SPBASIS, Val{2})) do I
                         =# - A[a, i, b, l]*B[a, j, a, k] + A[a, j, b, l]*B[b, i, a, k])
 end
 
-function white(Ω, H0)
+function white(Ω::TwoBodyARRAYOP, H0::TwoBodyARRAYOP)
     _, f, Γ = H(Ω, H0)
 
     Δ(i, k) = f[i, i] - f[k, k] + Γ[i, k, i, k]
@@ -66,37 +72,41 @@ function white(Ω, H0)
     _b2(i, j, k, l) = 4 \ (~isocc(i))*(~isocc(j))*isocc(k)*isocc(l) #=
                        =# * Γ[i, j, k, l] / Δ[i, j, k, l]
 
-    b1 = @Operator(SPBASIS) do i, j
+    b1 = FUNCOP{1}() do i, j
         _b1(i, j) - conj(_b1(j, i))
     end
-    b2 = @Operator(SPBASIS) do i, j, k, l
+    b2 = FUNCOP{2}() do i, j, k, l
         _b2(i, j, k, l) - conj(_b2(l, k, j, i))
     end
 
-    (0, b1, b2)
+    (zero(ARRAYOP{0}), tabulate(b1), tabulate(b2))
 end
 
 const comm = comm2
 const generator = white
 
-# Add methods for Operator addition, etc.
 function dΩ(Ω, params, s)
     H0, tol, batchsize = params
-    tot = (0.0im, zero(CArrayOperator{1}), zero(CArrayOperator{2}))
-    prev_ad = generator(Ω, H0) .|> tabulate
+    prev_tot = (0.0im, zero(ARRAYOP{1}), zero(ARRAYOP{2}))
+    prev_ad = generator(Ω, H0)
 
     # Do first term
+    tot = bernoulli(Float64, 0)/factorial(Float64(0)) .* prev_ad
 
     n = 1
-    while any(vecnorm.(tot - prev_tot) .> tol)
+    while sum(vecnorm.(tot - prev_tot)) tol
         prev_tot = tot
         tot += sum(n:n+batchsize-1) do i
-            bernoulli(Float64, i)/factorial(Float64(i))*(prev_ad = comm(Ω, prev_ad))
+            bernoulli(Float64, i)/factorial(Float64(i)) .* (prev_ad = comm(Ω, prev_ad))
         end
         n += batchsize
     end
 
     tot
+end
+
+const ALG = Euler()
+function main(Ω, params, s)
 end
 
 end # module MagnusIMSRG
