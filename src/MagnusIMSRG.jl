906885f4a62fa289f5e01a66bcf11ab49b893d20
Nicholas Todoroff <loppy1243@gmail.com>
2018-08-26 03:21:14 -0400

    Commit for posterity

--- src/MagnusIMSRG.jl
+++ src/MagnusIMSRG.jl
@@ -1,89 +1,104 @@
 module MagnusIMSRG
 
 using ManyBody
+import ManyBody.Operators
 using OrdinaryDiffEq
+using Combinatorics: levicivita
 using JuliaUtil: cartesian_pow, bernoulli
 
-const SPBASIS = Bases.Index{Bases.Pairing{4}}
-const REFSTATE = RefStates.Fermi{2, Bases.Pairing{4}}
+const SPBASIS = Bases.Pairing{4}
+const REFSTATE = RefStates.Fermi(Bases.Pairing{4}, 2)
 const MBBASIS = Bases.Paired{2, 4}
-const ELTYPE = Float64
-const ARRAYOP{N} = F64ArrayOperator{N, SPBASIS}
-const FUNCOP{N} = F64FunctionOperator{N, SPBASIS}
-const MBFUNCOP = F64FunctionOperator{1, MBBASIS}
+#const ELTYPE = Float64
+const ARRAYOP{N} = F64ArrayOperator{Bases.Product{N, NTuple{N, SPBASIS}}}
+const FUNCOP{N} = F64FunctionOperator{Bases.Product{N, NTuple{N, SPBASIS}}}
+const MBFUNCOP = F64FunctionOperator{MBBASIS}
 
 const LEVEL_SPACING = 1.0
 
 const TwoBodyARRAYOP = Tuple{ARRAYOP{0}, ARRAYOP{1}, ARRAYOP{2}}
 
 isocc(x) = ManyBody.isocc(REFSTATE, x)
+isunocc(x) = ManyBody.isocc(REFSTATE, x)
+normord(x) = ManyBody.normord(REFSTATE, x)
 
 nbody(A, n) = A[n-1]
-nbodyrep(A, n) = rep(nbody(A, n))
 
 comm2(A::TwoBodyARRAYOP, B::TwoBodyARRAYOP) =
-    (_comm0(BT, A, B), tabulate(_comm1(A, B)), tabulate(_comm2(A, B)))
-_comm0(A, B) = ARRAYOP{0}(fill(_comm0_2_2(nbodyrep(A, 2), nbodyrep(B, 2))))
-_comm1(A, B) = FUNCOP{1}() do i, j
-    _comm1_1_2(nbodyrep(A, 1), nbodyrep(B, 2), i, j) #=
- =# + _comm1_2_2(nbodyrep(A, 2), nbodyrep(B, 2), i, j) #=
- =# - _comm1_1_2(nbodyrep(B, 1), nbodyrep(A, 2), i, j)
+    (_comm0(BT, A, B), _comm1(A, B), _comm2(A, B))
+
+_comm0(A, B) = fill(_comm0_2_2(nbody(A, 2), nbody(B, 2)))
+function _comm1(A, B)
+    A0, A1, A2 = A
+    B0, B1, B2 = B
+
+    FUNCOP{1}() do i, j
+        _comm1_1_2(A1, B2, i, j) + _comm1_2_2(A2, B2, i, j) - _comm1_1_2(B1, A2, i, j)
+    end |> tabulate
 end
-_comm2(A, B) = FUNCOP{2}() do i, j, k, l
-    _comm2_1_2(nbodyrep(A, 1), nbodyrep(B, 2), i, j, k, l) #=
- =# + _comm2_2_2(nbodyrep(A, 2), nbodyrep(B, 2), i, j, k, l) #=
- =# + _comm2_1_2(nbodyrep(B, 1), nbodyrep(A, 2), i, j, k, l)
+function _comm2(A, B)
+    A0, A1, A2 = A
+    B0, B1, B2 = B
+
+    FUNCOP{2}() do I, J
+        _comm2_1_2(A1, B2, I, J) + _comm2_2_2(A2, B2, I, J) + _comm2_1_2(B1, A2, I, J)
+    end |> tabulate
 end
 
-_comm0_2_2(A, B) = 4 \ sum(cartesian_pow(SPBASIS, Val{4})) do I
-    a, b, c, d = I
-    isocc(a)*isocc*(b)*(~isocc(c))*(~isocc(d)) #=
- =# * (A[a, b, c, d]*B[c, d, a, b] - B[a, b, c, d]*A[c, d, a, b])
+
+_comm0_2_2(A, B) = 4 \ sum(matrixiter(A)) do X
+    I, J = X
+
+    isocc(I)*isunocc(J)*(A[I, J]*B[J, I] - B[I, J]*A[J, I])
 end
 
-_comm1_1_2(A, B, i, j) = sum(cartesian_pow(SPBASIS, Val{2})) do I
-    a, b = I
+_comm1_1_2(A, B, i, j) = sum(matrixiter(A)) do X
+    a, b = X
+
     (isocc(a) - isocc(b))*A[a, b]*B[b, i, a, j]
 end
-
 _comm1_2_2(A, B, i, j) = 2 \ sum(cartesian_pow(SPBASIS, Val{3})) do I
     a, b, c = I
-    (~isocc(a))*(~isocc(b))*isocc(c) + isocc(a)*isocc(b)*(~isocc(c)) #=
+    isunocc(a)*isunocc(b)*isocc(c) + isocc(a)*isocc(b)*isunocc(c) #=
  =# * (A[c, i, a, b]*B[a, b, c, j] - B[c, i, a, b]*A[a, b, c, j])
 end
 
-_comm2_1_2(A, B, i, j, k, l) = 4 \ sum(SPBASIS) do a
-    A[i, a]*B[a, j, k, l] - A[j, a]*B[a, i, k, l] #=
- =# - A[a, k]*B[i, j, a, l] + A[a, l]*B[i, j, a, k]
+function _comm2_1_2(A, B, I, J)
+    i, j = I; k, l = J
+    4 \ sum(SPBASIS) do a
+        A[i, a]*B[a, j, k, l] - A[j, a]*B[a, i, k, l] #=
+     =# - A[a, k]*B[i, j, a, l] + A[a, l]*B[i, j, a, k]
+    end
 end
-
-_comm2_2_2(A, B, i, j, k, l) = 4 \ sum(cartesian_pow(SPBASIS, Val{2})) do I
-    a, b = I
-    2 \ (1-isocc(a)-isocc(b)) * (A[i, j, a, b]*B[a, b, k, l] - B[i, j, a, b]*A[a, b, k, l]) #=
- =# + (isocc(a)-isocc(b))*(A[a, i, b, k]*B[b, j, a, l] - A[a, j, b, k]*B[b, i, a, l] #=
-                        =# - A[a, i, b, l]*B[a, j, a, k] + A[a, j, b, l]*B[b, i, a, k])
+function _comm2_2_2(A, B, I, J)
+    i, j = I; k, l = J
+    4 \ sum(cartesian_pow(SPBASIS, Val{2})) do I
+       a, b = I
+       2 \ (1-isocc(a)-isocc(b))*(A[i, j, a, b]*B[a, b, k, l] - B[i, j, a, b]*A[a, b, k, l]) #=
+    =# + (isocc(a)-isocc(b))*(A[a, i, b, k]*B[b, j, a, l] - A[a, j, b, k]*B[b, i, a, l] #=
+                           =# - A[a, i, b, l]*B[a, j, a, k] + A[a, j, b, l]*B[b, i, a, k])
+    end
 end
 
 function white(Ω::TwoBodyARRAYOP, H0::TwoBodyARRAYOP)
-    _, f, Γ = H(Ω, H0)
+    E0, f, Γ = H(Ω, H0)
 
     Δ(i, k) = f[i, i] - f[k, k] + Γ[i, k, i, k]
     Δ(i, j, k, l) =
         f[i, i] + f[j, j] - f[k, k] - f[l, l] + Γ[k, l, k, l] + Γ[i, j, i, j] #=
      =# - Γ[i, k, i, k] - Γ[j, l, j, l] - Γ[i, l, i, l] - Γ[j, k, j, k]
 
-    _b1(i, j) = ~(isocc(i))*isocc(j)*f[i, j] / Δ(i, j)
-    _b2(i, j, k, l) = 4 \ (~isocc(i))*(~isocc(j))*isocc(k)*isocc(l) #=
-                       =# * Γ[i, j, k, l] / Δ[i, j, k, l]
+    _b1(i, j) = isunocc(i)*isocc(j)*f[i, j] / Δ(i, j)
+    _b2(I, J) = 4 \ isunocc(I)*isocc(J) * Γ[I, J] / Δ(I..., J...)
 
-    b1 = FUNCOP{1}() do i, j
-        _b1(i, j) - conj(_b1(j, i))
+    b1 = FUNCOP{1}() do I, J
+        _b1(I..., J...) - conj(_b1(J..., I...))
     end
-    b2 = FUNCOP{2}() do i, j, k, l
-        _b2(i, j, k, l) - conj(_b2(l, k, j, i))
+    b2 = FUNCOP{2}() do I, J
+        _b2(I..., J...) - conj(_b2(J..., I...))
     end
 
-    (zero(ARRAYOP{0}), tabulate(b1), tabulate(b2))
+    (zero(E0), tabulate(b1), tabulate(b2))
 end
 
 const comm = comm2
@@ -91,7 +106,7 @@ const generator = white
 
 function dΩ(Ω, params, s)
     H0, tol, batchsize = params
-    prev_tot = (0.0im, zero(ARRAYOP{1}), zero(ARRAYOP{2}))
+    prev_tot = (0.0, zero(ARRAYOP{1}), zero(ARRAYOP{2}))
     prev_ad = generator(Ω, H0)
 
     # Do first term
@@ -110,21 +125,23 @@ function dΩ(Ω, params, s)
 end
 
 function im_pairingH(g)
-    E0_val = sum(occ(REFSTATE)) do i
+    E0 = sum(occ(REFSTATE)) do i
         LEVEL_SPACING*(level(i) - 1) - g/4*isocc(flipspin(i))
     end
-    E0 = FUNCOP{0}(() -> E0_val)
     f = FUNCOP{1}() do p, q
         p != q && return 0
         2 \ (LEVEL_SPACING*(level(p)-1) - isocc(p)*g/2)
     end
-    Γ = FUNCOP{2}() do p, q, r, s
-        mask = (level(p) == level(r)) #=
-            =# * (level(q) == level(s)) #=
-            =# * spinup(p)*spinup(r) #=
-            =# * spindown(q)*spindown(s)
-
-        -g/2*mask
+    Γ = FUNCOP{2}() do I, J
+        sgn1 = index(I) |> Tuple |> collect |> sortperm |> levicivita
+        sgn2 = index(J) |> Tuple |> collect |> sortperm |> levicivita
+        X = Bases.Slater{SPBASIS}(I...)
+        Y = Bases.Slater{SPBASIS}(J...)
+        -g/2*sgn1*sgn2 * sum(cartesian_pow(1:nlevels(SPBASIS), Val{2})) do Z
+            p, q, r, s = SPBASIS.((Z[1], Z[1], Z[2], Z[2]),
+                                  (SPINUP, SPINDOWN, SPINUP, SPINDOWN))
+            X'Operators.A(p', q', s, r)(Y)
+        end
     end
 
     (E0, f, Γ)
@@ -134,26 +151,18 @@ function to_mbop(op)
     E0, f, Γ = op
 
     MBFUNCOP() do Y, X
-        b0 = E0[]*(Y'X)
-        b1 = sum(cartesian_pow(SPBASIS, Val{2})) do I
-            p, q = I
-            sgn, NA = normord(A(p', q))
-            f[p, q]*sgn*(Y'NA(X))
+        b0 = E0*(Y'X)
+        b1 = sum(matrixiter(f)) do Z
+            p, q = Z[1][1], Z[2][1]
+            NA = normord(Operators.A(p', q))
+            f[p, q]*(Y'NA(X))
         end
-        b2 = sum(cartesian_pow(SPBASIS, Val{4})) do I
-            p, q, r, s = I
-            sgn, NA = normord(A(p', q))
-            val3 = NA(X)
-            val = Γ[p, q, r, s]
-            val2 = val*sgn*(Y'val3)
-            if !iszero(val)
-                @show inner(Y)'inner(val3)
-#                print(map(inner, I), " --> ")
-#                println(sgn, ", ", val, ", ", val2)
-            end
-            val2
+        b2 = sum(matrixiter(Γ)) do Z
+            I, J = Z
+            p, q = I; r, s = J
+            NA = normord(Operators.A(p', q', r, s))
+            Γ[I, J]*(Y'NA(X))
         end
-        println()
 
         b0 + b1 + b2
     end
