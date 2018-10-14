de1165dbf3cb189f4926100d8fd5050143be6705
Nicholas Todoroff <loppy1243@gmail.com>
2018-10-13 23:23:48 -0400

    Correct comm2() antihermiticity test, all tests pass

--- test/comm.jl
+++ test/comm.jl
@@ -19,13 +19,17 @@ commtest(; atol) = @testset "Commutators" begin
     @test all(abs.(mat_Γ.rep .- pw_Γ.rep) .< atol)
 
     f_1 = 2 \ (f_1 + f_1')
-    Γ_1 = 2 \ (Γ_1 + PermutedDimsArray(Γ_1, [4, 3, 2, 1]))
+    f_2 = 2 \ (f_2 + f_2')
+    Γ_1 = 2 \ (Γ_1 + PermutedDimsArray(Γ_1, [3, 4, 1, 2]))
+    Γ_2 = 2 \ (Γ_2 + PermutedDimsArray(Γ_2, [3, 4, 1, 2]))
     op_1 = (E0_1, ARRAYOP(1)(f_1), ARRAYOP(2)(Γ_1))
+    op_2 = (E0_2, ARRAYOP(1)(f_2), ARRAYOP(2)(Γ_2))
 
     mat_E, mat_f, mat_Γ = comm2(op_1, op_2)
 
-    @test all(@show(abs.(mat_f.rep + mat_f.rep')) .< atol)
-    @test all(abs.(mat_Γ.rep + PermutedDimsArray(mat_Γ.rep, [4, 3, 2, 1])) .< atol)
+    @test abs(mat_E) < atol
+    @test all(abs.(mat_f.rep + mat_f.rep') .< atol)
+    @test all(abs.(mat_Γ.rep + PermutedDimsArray(mat_Γ.rep, [3, 4, 1, 2])) .< atol)
 
     E0_1 = 0.0
     f_1 .= 0.0
