05e48a954af98512155948fb2aa7075e49fb2775
Nicholas Todoroff <loppy1243@gmail.com>
2018-10-15 10:37:53 -0400

    Clean up commtest() using new convenience functions

--- test/comm.jl
+++ test/comm.jl
@@ -1,15 +1,8 @@
-using MagnusIMSRG: DIM, comm2, comm2_pw, ARRAYOP, to_mbop
+using MagnusIMSRG: DIM, comm2, comm2_pw, randop, ZERO_OP, hermiticize
 
 commtest(; atol) = @testset "Commutators" begin
-    E0_1 = rand(Float64)
-    E0_2 = rand(Float64)
-    f_1 = rand(Float64, DIM, DIM)
-    f_2 = rand(Float64, DIM, DIM)
-    Γ_1 = rand(Float64, DIM, DIM, DIM, DIM)
-    Γ_2 = rand(Float64, DIM, DIM, DIM, DIM)
-
-    op_1 = (E0_1, ARRAYOP(1)(f_1), ARRAYOP(2)(Γ_1))
-    op_2 = (E0_2, ARRAYOP(1)(f_2), ARRAYOP(2)(Γ_2))
+    op_1 = randop()
+    op_2 = randop()
 
     mat_E, mat_f, mat_Γ = comm2(op_1, op_2)
     pw_E, pw_f, pw_Γ = comm2_pw(op_1, op_2)
@@ -18,12 +11,8 @@ commtest(; atol) = @testset "Commutators" begin
     @test all(abs.(mat_f.rep .- pw_f.rep) .< atol)
     @test all(abs.(mat_Γ.rep .- pw_Γ.rep) .< atol)
 
-    f_1 = 2 \ (f_1 + f_1')
-    f_2 = 2 \ (f_2 + f_2')
-    Γ_1 = 2 \ (Γ_1 + PermutedDimsArray(Γ_1, [3, 4, 1, 2]))
-    Γ_2 = 2 \ (Γ_2 + PermutedDimsArray(Γ_2, [3, 4, 1, 2]))
-    op_1 = (E0_1, ARRAYOP(1)(f_1), ARRAYOP(2)(Γ_1))
-    op_2 = (E0_2, ARRAYOP(1)(f_2), ARRAYOP(2)(Γ_2))
+    op_1 = hermiticize(op_1)
+    op_2 = hermiticize(op_2)
 
     mat_E, mat_f, mat_Γ = comm2(op_1, op_2)
 
@@ -31,13 +20,7 @@ commtest(; atol) = @testset "Commutators" begin
     @test all(abs.(mat_f.rep + mat_f.rep') .< atol)
     @test all(abs.(mat_Γ.rep + PermutedDimsArray(mat_Γ.rep, [3, 4, 1, 2])) .< atol)
 
-    E0_1 = 0.0
-    f_1 .= 0.0
-    Γ_1 .= 0.0
-
-    op_1 = (E0_1, ARRAYOP(1)(f_1), ARRAYOP(2)(Γ_1))
-
-    mbop_1 = to_mbop(op_1) |> tabulate
+    op_1 = ZERO_OP
 
     mat_E, mat_f, mat_Γ = comm2(op_1, op_2)
 
