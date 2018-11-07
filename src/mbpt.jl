b02de15b3f4bcab6848106f1cbd5d8facdbb4055
Nicholas Todoroff <loppy1243@gmail.com>
2018-11-06 22:47:58 -0500

    Fix mbpt2() expression
    f[p, p] terms were missing a minus.

--- src/mbpt.jl
+++ src/mbpt.jl
@@ -2,6 +2,6 @@ function mbpt2(op::TwoBodyARRAYOP)
     _, f, Γ = op
 
     sum(Iterators.product(HOLES, HOLES, PARTS, PARTS)) do (i, j, a, b)
-        4 \ Γ[a, b, i, j]^2 / (f[i, i] + f[j, j] + f[a, a] + f[b, b])
+        4 \ Γ[a, b, i, j]^2 / (f[i, i] + f[j, j] - f[a, a] - f[b, b])
     end
 end
