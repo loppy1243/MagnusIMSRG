aeb0ef700d3f50c0ac374b986f6866bb386870a8
Nicholas Todoroff <loppy1243@gmail.com>
2018-11-06 23:57:26 -0500

    Ignore contributions to white() generator not satisfying the occupation expression

--- src/Generators/main.jl
+++ src/Generators/main.jl
@@ -7,7 +7,7 @@ SIGNAL_OPS && include("../signalops.jl")
 let EMIT_ZERO_WARNING_1=true, EMIT_ZERO_WARNING_2=true
 global white
 function white(Ω::TwoBodyARRAYOP, h::TwoBodyARRAYOP)
-    E0, f, Γ = h
+    E, f, Γ = h
 
     Δ(i, k) = f[i, i] - f[k, k] + Γ[i, k, i, k]
     Δ(i, j, k, l) =
@@ -16,26 +16,33 @@ function white(Ω::TwoBodyARRAYOP, h::TwoBodyARRAYOP)
 
     function _b1(I, J)
         d = Δ(I..., J...)
-        if abs(d) < E_DENOM_ATOL
+        mask = isunocc(I)*isocc(J)
+        if !iszero(mask) && abs(d) < E_DENOM_ATOL
             if EMIT_ZERO_WARNING_1
                 @warn("One-body energy denominator is zero! Will not warn again.")
                 EMIT_ZERO_WARNING_1 = false
             end
             zero(d)
+        elseif iszero(mask)
+            zero(d)
         else
-            isunocc(I)*isocc(J)*f[I, J] / d
+            mask*f[I, J] / d
         end
     end
+
     function _b2(I, J)
         d = Δ(I..., J...)
-        if abs(d) < E_DENOM_ATOL
+        mask = all(isunocc, I)*all(isocc, J)
+        if !iszero(mask) && abs(d) < E_DENOM_ATOL
             if EMIT_ZERO_WARNING_2
                 @warn("Two-body energy denominator is zero! Will not warn again.")
                 EMIT_ZERO_WARNING_2 = false
             end
             zero(d)
+        elseif iszero(mask)
+            zero(d)
         else
-            4 \ all(isunocc, I)*all(isocc, J)*Γ[I, J] / d
+            4 \ mask*Γ[I, J] / d
         end
     end
 
@@ -47,7 +54,7 @@ function white(Ω::TwoBodyARRAYOP, h::TwoBodyARRAYOP)
         _b2(I, J) - conj(_b2(J, I))
     end
 
-    (zero(E0), tabulate(b1), tabulate(b2))
+    (zero(E), tabulate(b1), tabulate(b2))
 end end
 
 end # module Generators
