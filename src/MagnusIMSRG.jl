9e5566dabf5b9bd10c09c003f7a288be83bf6bea
Nicholas Todoroff <loppy1243@gmail.com>
2018-11-20 20:31:09 -0500

    Commit for posterity

--- src/MagnusIMSRG.jl
+++ src/MagnusIMSRG.jl
@@ -14,7 +14,40 @@ const SIGNAL_OPS = false
 
 ### Parameters ###############################################################################
 ##############################################################################################
-@with_kw struct HyParams
+#@with_kw struct HyParams
+#    ENG_DENOM_ATOL  ::Float64                  = 1e-5
+#
+#    Ω_RTOL          ::Float64                  = 0.0
+#    Ω_ATOL          ::Float64                  = 0.01
+#    Ω_BATCHSIZE     ::Float64                  = 5
+#
+#    H_RTOL          ::Float64                  = 0.0
+#    H_ATOL          ::Float64                  = 0.01
+#    H_BATCHSIZE     ::Int                      = 5
+#
+#    INT_RTOL        ::Float64                  = 1e-8
+#    INT_ATOL        ::Float64                  = 1e-3
+#    INT_DIV_RTHRESH ::Float64                  = 1.0
+#    MAX_INT_ITERS   ::Int                      = 100
+#
+#    ELTYPE          ::Type                     = Float64
+#    MBBASIS         ::Type                     = Bases.Paired{4, 4}
+#    SPBASIS         ::Type                     = spbasis(MBBASIS)
+#    DIM             ::Int                      = dim(spbasis)
+#    REFSTATE        ::Union{RefState, MBBasis} = RefStates.Fermi{SPBASIS}(2)
+#    HOLES           ::Vector{SPBASIS}          = holes(REFSTATE)
+#    PARTS           ::Vector{SPBASIS}          = parts(REFSTATE)
+#end
+
+@with_kw struct Params
+    S_LARGE_STEP         ::Float64  = 1.0
+    S_SMALL_STEP         ::Float64  = 0.1
+    TRUNC_LEVEL          ::Int      = 2
+    H                    ::Function
+    H_PARAMS             ::Union{Tuple, NamedTuple}
+    comm                 ::Function = comm2
+    gen                  ::Function = Generators.white
+#    HYPARAMS             ::HyParams = HyParams()
     ENG_DENOM_ATOL  ::Float64                  = 1e-5
 
     Ω_RTOL          ::Float64                  = 0.0
@@ -32,32 +65,11 @@ const SIGNAL_OPS = false
 
     ELTYPE          ::Type                     = Float64
     MBBASIS         ::Type                     = Bases.Paired{4, 4}
-    REFSTATE        ::Union{RefState, MBBasis} = RefStates.Fermi{Bases.Pairing{4}}(2)
-    OPTYPE          ::Union{Type, Nothing}     = nothing
-end
-Base.getproperty(x::HyParams, s::Symbol) =
-    if s === :SPBASIS
-        spbasis(supbasis(getfield(x, :MBBASIS)))
-    elseif s === :DIM
-        dim(spbasis(supbasis(getfield(x, :MBBASIS))))
-    elseif s === :HOLES || s === :OCC
-        holes(getfield(x, :REFSTATE))
-    elseif s === :PARTS || s === :UNOCC
-        parts(getfield(x, :REFSTATE))
-    elseif s === :OPTYPE && getfield(x, s) === nothing
-        DenseIMArray{2, spbasis(supbasis(getfield(x, :MBBASIS)))}
-    else
-        getfield(x, s)
-    end
-
-@with_kw struct Params
-    S_LARGE_STEP         ::Float64  = 1.0
-    S_SMALL_STEP         ::Float64  = 0.1
-    H                    ::Function
-    H_PARAMS             ::NamedTuple
-    comm                 ::Function = comm2
-    gen                  ::Function = Generators.white
-    HYPARAMS             ::HyParams
+    SPBASIS         ::Type                     = spbasis(MBBASIS)
+    DIM             ::Int                      = dim(spbasis)
+    REFSTATE        ::Union{RefState, MBBasis} = RefStates.Fermi{SPBASIS}(2)
+    HOLES           ::Vector{SPBASIS}          = holes(REFSTATE)
+    PARTS           ::Vector{SPBASIS}          = parts(REFSTATE)
 end
 
 SIGNAL_OPS && include("signalops.jl")
@@ -70,6 +82,8 @@ include("mbpt.jl")
 
 factorial(T::Type{<:Number}, n::Integer) = prod(one(T):convert(T, n))
 
+choosetol(tols...) = minimum(tol in tols if !iszero(tol))
+
 function dΩ(Ω, params, h)
     @debug "Entering dΩ"
     @unpack gen, comm, Ω_ATOL, Ω_RTOL, Ω_BATCHSIZE = params
@@ -80,7 +94,7 @@ function dΩ(Ω, params, h)
     tot = bernoulli(Float64, 0)/factorial(Float64, 0) * prev_ad
 
     n = 1
-    while norm(tot - prev_tot) > max(Ω_ATOL, Ω_RTOL*norm(tot))
+    while norm(tot - prev_tot) > choosetol(Ω_ATOL, Ω_RTOL*norm(tot))
         prev_tot = tot
         tot += sum(n:n+Ω_BATCHSIZE-1) do i
             bernoulli(Float64, i)/factorial(Float64, i) * (prev_ad = comm(Ω, prev_ad))
@@ -92,26 +106,29 @@ function dΩ(Ω, params, h)
 end
 
 solve(H, h_params; kws...) = solve((xs...,) -> nothing, H, h_params; kws...)
-function solve(cb, params)
-    @unpack H, H_PARAMS, OPTYPE, 
+function solve(cb, H, H_PARAMS; kws...)
+    params = Params(; H=H, H_PARAMS=H_PARAMS, kws...)
+    @unpack TRUNC_LEVEL, ELTYPE, INT_ATOL, INT_RTOL, MAX_INT_ITERS, INT_DIV_ATOL, INT_DIV_RTOL = params
+    ZERO_OP = zero(DenseIMArrayOp{TRUNC_LEVEL, ELTYPE})
 
     s = 0.0
     n = 0
-    Ω = zero(OPTYPE)
-    h_prev = zero(OPTYPE)
-    h = 
+    Ω = ZERO_OP
+    h_prev = ZERO_OP
+    h = H(H_PARAMS...)
     dE_2 = mbpt2(h)
 
-    while (ratio = abs(dE_2/nbody(h, 0))) > INT_RTOL
+    while abs(dE_2) > choosetol(INT_ATOL, INT_RTOL*abs(h.parts[0]))
+        ratio = dE_2/h.parts[0]
         if n >= max_int_iters
             @warn "Iteration maximum exceeded in solve()" n s
             break
         end
-        if ratio > INT_DIV_RTHRESH
+        if abs(dE_2) > choosetol(INT_DIV_ATOL, INT_DIV_RTOL*h.parts[0])
             @warn "Divergence threshold exceeded in solve()" n s ratio
             break
         end
-        print_info && _solve_print_info(n, max_int_iters, nbody(h, 0), dE_2, ratio)
+        print_info && _solve_print_info(n, max_int_iters, h.parts[0], dE_2, ratio)
         cb(s, Ω, h, dE_2)
 
         h_prev = h
