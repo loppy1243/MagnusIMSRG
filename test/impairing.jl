e96ec91b633c9876b31dd20decf99d7fd6a7b660
Nicholas Todoroff <loppy1243@gmail.com>
2018-11-26 12:02:52 -0500

    Add fullmatrix option to impairingtest()
    All tests pass

--- test/impairing.jl
+++ test/impairing.jl
@@ -1,24 +1,18 @@
 import MagnusIMSRG.Hamiltonians: impairing
 import ManyBody.Hamiltonians: pairing
 
-impairingtest(; atol) = @testset "IMPairing Hamiltonian" begin
+impairingtest(; atol, fullmatrix) = @testset "IMPairing Hamiltonian" begin
     SPB = Bases.Pairing{4}
-    MB = Bases.Paired{4, 4}
     REF = RefStates.Fermi{SPB}(2)
 
+    MB = if fullmatrix
+        Bases.Paired{4, 4}
+    else
+        Bases.MBPairing{4, 4}
+    end
     h_true = tabulate(pairing(1, 0.5), Array{Float64}, 2, MB)
     h_im = tabulate(impairing(REF, 1, 0.5), IMArrayOp{2, Float64},
                     (Array, 2, SPB), (Array, 4, SPB))
     h_mb = mbop(h_im, REF, MB, MB)
-
-    diff = h_mb - h_true
-
-    print("Correct: ")
-    show(stdout, "text/plain", h_true); println(); println()
-    print("From impairing(): ")
-    show(stdout, "text/plain", h_mb); println(); println()
-    print("Difference: ")
-    show(stdout, "text/plain", diff); println()
-
-    @test all(x -> abs(x) < atol, diff)
+    @test all(x -> abs(x) < atol, h_mb - h_true)
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,5 @@ using Test
 using ManyBody, MagnusIMSRG, MagnusIMSRG.IMOperators
 const IMOps = IMOperators
 
-#include("comm.jl"); commtest(atol=1e-5)
-include("impairing.jl"); impairingtest(atol=1e-5)
+include("comm.jl");      commtest(atol=1e-5)
+include("impairing.jl"); impairingtest(atol=1e-5, fullmatrix=false)
