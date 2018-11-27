6602e1d6b21d194febf0a999f51db11dae478028
Nicholas Todoroff <loppy1243@gmail.com>
2018-11-27 13:54:58 -0500

    Add test for Hermiticity of Hamiltonians.impairing()

--- test/impairing.jl
+++ test/impairing.jl
@@ -4,7 +4,6 @@ import MagnusIMSRG.Hamiltonians: impairing
 import ManyBody.Hamiltonians: pairing
 
 impairingtest(; atol, fullmatrix) = @testset "IMPairing Hamiltonian" begin
-    @debug "Testing Correctness"
     SPB = Bases.Pairing{4}
     DIM = dim(SPB)
     REF = RefStates.Fermi{SPB}(2)
@@ -14,15 +13,21 @@ impairingtest(; atol, fullmatrix) = @testset "IMPairing Hamiltonian" begin
     else
         Bases.Paired{4, 4}
     end
-    h_true = tabulate(pairing(1, 0.5), Array{Float64}, 2, MB)
     h_im = tabulate(impairing(REF, 1, 0.5), IMArrayOp{2, Float64},
                     (Array, 2, SPB), (Array, 4, SPB))
-    h_mb = mbop(h_im, REF, MB, MB)
-    @test all(x -> abs(x) < atol, h_mb - h_true)
 
-    @debug "Testing anti-symmetry"
+    @testset "Correctness" begin
+        h_true = tabulate(pairing(1, 0.5), Array{Float64}, 2, MB)
+        h_mb = mbop(h_im, REF, MB, MB)
+        @test all(x -> abs(x) < atol, h_mb - h_true)
+    end
+
     arr = reshape(h_im.parts[2], DIM, DIM, DIM, DIM)
-    for I in CartesianIndices(arr)
+    @testset "Hermiticity" for I in CartesianIndices(arr)
+        i, j, k, l = Tuple(I)
+        @test arr[I] == conj(arr[k, l, i, j])
+    end
+    @testset "Anti-symmetry" for I in CartesianIndices(arr)
         for perm in permutations([1, 2, 3, 4])
             J = CartesianIndex(Tuple(I)[perm])
             @test arr[I] == levicivita(perm)*arr[J]
