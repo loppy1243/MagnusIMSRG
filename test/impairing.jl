ec611cf4161329c9ff60467065e0dfe186171c3f
Nicholas Todoroff <loppy1243@gmail.com>
2018-11-27 13:36:55 -0500

    Add antisymmetry test for Hamiltonians.impairing()

--- test/impairing.jl
+++ test/impairing.jl
@@ -1,8 +1,12 @@
+using Combinatorics: permutations, levicivita
+
 import MagnusIMSRG.Hamiltonians: impairing
 import ManyBody.Hamiltonians: pairing
 
 impairingtest(; atol, fullmatrix) = @testset "IMPairing Hamiltonian" begin
+    @debug "Testing Correctness"
     SPB = Bases.Pairing{4}
+    DIM = dim(SPB)
     REF = RefStates.Fermi{SPB}(2)
 
     MB = if fullmatrix
@@ -15,4 +19,13 @@ impairingtest(; atol, fullmatrix) = @testset "IMPairing Hamiltonian" begin
                     (Array, 2, SPB), (Array, 4, SPB))
     h_mb = mbop(h_im, REF, MB, MB)
     @test all(x -> abs(x) < atol, h_mb - h_true)
+
+    @debug "Testing anti-symmetry"
+    arr = reshape(h_im.parts[2], DIM, DIM, DIM, DIM)
+    for I in CartesianIndices(arr)
+        for perm in permutations([1, 2, 3, 4])
+            J = CartesianIndex(Tuple(I)[perm])
+            @test arr[I] = levicivita(perm)*arr[J]
+        end
+    end
 end
