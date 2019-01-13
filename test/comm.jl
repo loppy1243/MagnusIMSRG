4209bcb6091f10f8eb4fb6d3c59b137998f82fee
Nicholas Todoroff <loppy1243@gmail.com>
2019-01-12 22:25:39 -0500

    Generalize test/comm.jl
    Now supports any number of commutators, all of which are compared and
    tested for Hermiticity and the zero property piece by piece.

--- test/comm.jl
+++ test/comm.jl
@@ -1,6 +1,8 @@
 using MagnusIMSRG.IMOperators
 using MagnusIMSRG.Commutators: comm2, comm2_pw
 
+const TWO_BODY_COMMS = [comm2, comm2_pw]
+
 commtest(; atol) = @testset "Commutators" begin
     MagnusIMSRG.@localgetparams SPBASIS
     dims(N) = Tuple(d for _=1:N for d in fulldims(SPBASIS))
@@ -8,18 +10,27 @@ commtest(; atol) = @testset "Commutators" begin
     op1 = randimop(Float64, dims(2), dims(4))
     op2 = randimop(Float64, dims(2), dims(4))
 
-    within_tol(x) = all(y -> abs(y) < atol, x)
+    test_within_tol(x) = @testset "$(n-1)-body piece" for (n, part) in enumerate(x.parts)
+        @test all(abs.(x) .< atol)
+    end
 
-    mat_comm = comm2(op1, op2)
-    pw_comm = comm2_pw(op1, op2)
-    @test within_tol(mat_comm - pw_comm)
+    @testset "$(TWO_BODY_COMMS[i]) vs. $(TWO_BODY_COMMS[j])" #=
+ =# for i in eachindex(TWO_BODY_COMMS), j = i+1:lastindex(TWO_BODY_COMMS)
+        commA_val = TWO_BODY_COMMS[i](op1, op2)
+        commB_val = TWO_BODY_COMMS[j](op1, op2)
+
+        test_within_tol(commA_val - commB_val)
+    end
 
     op1 = (op1 + op1')/2
     op2 = (op2 + op2')/2
-    mat_comm = comm2(op1, op2)
-    @test within_tol((mat_comm + mat_comm')/2)
+    @testset "Hermiticiy ($comm_func)" for comm_func in TWO_BODY_COMMS
+        comm_val = comm_func(op1, op2)
+        test_within_tol((comm_val + comm_val')/2)
+    end
 
     op1 = zero(op1)
-    mat_comm = comm2(op1, op2)
-    @test within_tol(mat_comm)
+    @testset "Zero ($comm_func)" for comm_func in TWO_BODY_COMMS
+        test_within_tol(comm_func(op1, op2))
+    end
 end
